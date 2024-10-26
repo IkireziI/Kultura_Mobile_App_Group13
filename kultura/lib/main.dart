c8d397e65a1aa7f56c555eec4caefc5a2a0655de
IkireziI <i.ikirezi@alustudent.com>
2024-10-26 20:16:44 +0200

    marketplace work

--- kultura/lib/main.dart
+++ kultura/lib/main.dart
@@ -8,7 +8,7 @@ import 'pages/music_course.dart';
 import 'pages/painting_course.dart';
 import 'pages/literature_course.dart';
 import 'pages/profile.dart';
-import 'pages/market_painting.dart';
+import 'pages/marketplace.dart';
 import 'pages/market_music.dart';
 import 'pages/market_literature.dart';
 
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple, // Set the primary color theme to purple
       ),
-      initialRoute: '/login', // Set the initial route to home
+      initialRoute: '/home', // Set the initial route to home
       routes: {
         // Authentication routes
         '/login': (context) => const LoginScreen(),
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
 
         '/home': (context) => const Home(), // Route for the home screen
 
-        'market_painting': (context) => const MarketplacePainting(), // Route for the Marketplace (Painting)
+        '/marketplace': (context) => const MarketplacePage(), // Route for the Marketplace (Painting)
         'market_music': (context) => const MarketplaceMusic(), // Route for the Marketplace (Music)
         'market_literature': (context) => const MarketplaceLiterature(), // Route for the Marketplace (Literature)
 
---
+++ kultura/lib/pages/settings.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SettingsScreen());
+}
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SettingsPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          // Header with image and title
+          Container(
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage('assets/kultura_logo.png'),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+
+                  SizedBox(height: 10), // Space between image and title
+
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          // Settings options
+          ListTile(
+            leading: const Icon(Icons.manage_accounts, color: Colors.grey),
+            title: const Text('Manage Account', style: TextStyle(fontSize: 18)),
+            onTap: () {
+              // Handle tap
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.lock, color: Colors.grey),
+            title:
+            const Text('Change Password', style: TextStyle(fontSize: 18)),
+            onTap: () {
+              // Handle tap
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.help, color: Colors.grey),
+            title: const Text('Help & Support', style: TextStyle(fontSize: 18)),
+            onTap: () {
+              // Handle tap
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.security, color: Colors.grey),
+            title: const Text('Privacy and security',
+                style: TextStyle(fontSize: 18)),
+            onTap: () {
+              // Handle tap
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
